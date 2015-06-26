ff1568c46f655134bb2e8ac303b61e94564b94bb
Erto Pena <ertopena@gmail.com>
2015-06-26 02:50:20 -0500

    Add files to test expansions of Fungus functionality.

---
+++ Assets/Scripts/Interfaces/ISeeable.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface ISeeable
+{
+	event Phone.Controller.GameEvent OnSeen;
+}
---
+++ Assets/Scripts/Testing/CheckForVisible.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Testing
+{
+	/*
+	 * Testing this script on UI elements revealed that the OnBecameVisible event does not fire when
+	 * UI elements enter the screen. DO NOT USE.
+	 */ 
+	
+	
+	public class CheckForVisible : MonoBehaviour, ISeeable
+	{
+		public event Phone.Controller.GameEvent OnSeen;
+		public bool hasBeenSeen = false;
+
+
+		void OnBecameVisible()
+		{
+			hasBeenSeen = true;
+			
+			if (OnSeen != null)
+				OnSeen();
+		}
+	}
+}
